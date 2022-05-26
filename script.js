b00e27cef6f2971f9bbe09f28c178b4e9a6538ee
iamEvgen <evgen.alexa@gmail.com>
2022-05-26 17:28:44 +0300

    add forecast on page

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 (function () {
   let startCity = 'London';
 
+  //FORM
   const form = document.querySelector('form');
   const inputField = document.querySelector('.inputField');
 
+  //DATA FOR USER
+  const cityForUser = document.querySelector('.city');
+  const temperature = document.querySelector('.temperature');
+  const feelsLike = document.querySelector('.feelsLike');
+  const weatherIcon = document.querySelector('.weatherIcon');
+  const windSpeed = document.querySelector('.windSpeed');
+  const humidity = document.querySelector('.humidity');
+  const pressure = document.querySelector('.pressure');
+
   function extractWeatherForecats(response) {
     const city = response.name;
-    // const city = response.
+    const temp = Math.round(response.main.temp - 273.15);
+    const feelsLike = Math.round(response.main['feels_like'] - 273.15);
+    const pressure = response.main.pressure;
+    const weatherMain = response.weather[0].main;
+    const weatherDescription = response.weather[0].description;
+    const icon = response.weather[0].icon;
+    const windSpeed = response.wind.speed;
+    const humidity = response.main.humidity;
+    return {
+      city,
+      temp,
+      feelsLike,
+      pressure,
+      weatherMain,
+      weatherDescription,
+      icon,
+      windSpeed,
+      humidity,
+    };
+  }
+
+  function resetForm() {
+    inputField.value = '';
   }
 
   async function getWeatherForecast() {
@@ -18,24 +50,31 @@
       const response = await fetch(
         `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
       );
-      response.json().then((result) => {
-        console.log(result);
-      });
+      return response.json();
     } catch {
       // TODO add error message for user
       console.log('city is not found');
     }
   }
 
-  async function updatePage() {
-    const forecast = await getWeatherForecast();
-    // console.log('line33');
-    // console.log(forecast);
-    // const someData = forecast.then(result => console.log(result));
-    // console.log(someData);
-    // const forecastObject = extractWeatherForecats(forecast);
+  function renderForecast(forecastObject) {
+    cityForUser.textContent = forecastObject.city;
+    temperature.textContent = forecastObject.temp;
+    feelsLike.textContent = forecastObject.feelsLike;
+    windSpeed.textContent = forecastObject.windSpeed;
+    humidity.textContent = forecastObject.humidity;
+    pressure.textContent = forecastObject.pressure;
   }
 
-  getWeatherForecast('London');
+  async function updatePage() {
+    const forecast = await getWeatherForecast();
+    console.log(forecast);
+    const forecastObject = extractWeatherForecats(forecast);
+    renderForecast(forecastObject);
+    resetForm();
+  }
+
+  // getWeatherForecast('London');
+  updatePage();
   form.addEventListener('submit', updatePage);
 })();
